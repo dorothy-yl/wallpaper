92d8ec012a530ceb1c494de30bf5f6fe266ac9d3
dorothy <dorothy.luluya@gmail.com>
2025-09-17 12:21:23 +0800

    实现页面跳转

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -13,7 +13,7 @@
 		
 		<view class="section">
 			<view class="list">
-				<view class="row">
+				<navigator url="/pages/classlist/classlist" class="row">
 					<view class="left">
 						<uni-icons type="download-filled" size="20" ></uni-icons>
 						<view class="text">我的下载</view>
@@ -22,9 +22,9 @@
 						<view class="text">33</view>
 						<uni-icons type="right" size="15" color="#aaa"></uni-icons>
 					</view>
-				</view>
+				</navigator>
 				
-				<view class="row">
+				<navigator url="/pages/classlist/classlist" class="row">
 					<view class="left">
 						<uni-icons type="star-filled" size="20"></uni-icons>
 						<view class="text">我的评分</view>
@@ -33,7 +33,7 @@
 						<view class="text">33</view>
 						<uni-icons type="right" size="15" color="#aaa"></uni-icons>
 					</view>
-				</view>
+				</navigator>
 				
 				<view class="row">
 					<view class="left">
